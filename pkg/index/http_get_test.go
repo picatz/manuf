4b819ddfa5f125bd70a982e56529545cce09e540
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2022-02-06 13:48:36 -0500

    Add `RawGitHubURL` as a `PublicListingURL`
    Much faster, seemingly more reliable, and served over HTTPS

--- pkg/index/http_get_test.go
+++ pkg/index/http_get_test.go
@@ -37,3 +37,33 @@ func TestHTTPGetAllRecordsAndWriteToFile(t *testing.T) {
 	}
 	t.Logf("got %d records from file", len(cachedRecords))
 }
+
+func TestHTTPGet(t *testing.T) {
+	allRecords, err := HTTPGetRecords(context.Background(), http.DefaultClient, string(RawGitHubURL))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Logf("got %d records from HTTP requests", len(allRecords))
+
+	tmpDir := os.TempDir()
+
+	path := filepath.Join(tmpDir, "records.csv")
+
+	err = allRecords.Write(path)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	t.Cleanup(func() {
+		err := os.Remove(path)
+		if err != nil {
+			t.Logf("failed to cleanup temporary file %q: %v", path, err)
+		}
+	})
+
+	cachedRecords, err := RecordsFromFile(path)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Logf("got %d records from file", len(cachedRecords))
+}
--- pkg/index/urls.go
+++ pkg/index/urls.go
@@ -3,9 +3,10 @@ package index
 type PublicListingURL string
 
 const (
-	OUIURL   PublicListingURL = "http://standards-oui.ieee.org/oui/oui.csv"
-	CIDURL   PublicListingURL = "http://standards-oui.ieee.org/cid/cid.csv"
-	IABURL   PublicListingURL = "http://standards-oui.ieee.org/iab/iab.csv"
-	MAMURL   PublicListingURL = "http://standards-oui.ieee.org/oui28/mam.csv"
-	OUI36URL PublicListingURL = "http://standards-oui.ieee.org/oui36/oui36.csv"
+	OUIURL       PublicListingURL = "http://standards-oui.ieee.org/oui/oui.csv"
+	CIDURL       PublicListingURL = "http://standards-oui.ieee.org/cid/cid.csv"
+	IABURL       PublicListingURL = "http://standards-oui.ieee.org/iab/iab.csv"
+	MAMURL       PublicListingURL = "http://standards-oui.ieee.org/oui28/mam.csv"
+	OUI36URL     PublicListingURL = "http://standards-oui.ieee.org/oui36/oui36.csv"
+	RawGitHubURL PublicListingURL = "https://raw.githubusercontent.com/picatz/manuf/main/manuf.csv"
 )
